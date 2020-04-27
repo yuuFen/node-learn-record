d80e3526dcae398cc261a319c57f38fb363d6ce8
Yu <me@yuufen.com>
2020-04-27 23:44:03 +0800

    注释

--- 02-socket/chatServer.js
+++ 02-socket/chatServer.js
@@ -1,3 +1,12 @@
+// 原理：
+// Net 模块提供一个异步 API，能够创建基于流的 TCP 服务器，
+// 客户端与服务器建立连接后，服务器可以获得一个全双工 Socket 对象，
+// 服务器可以保存 Socket 对象列表，
+// 在接受某客户端消息时，推送给其他客户端。
+//
+// 通过 Telnet 连接服务器
+// telnet localhost 9000
+
 const net = require('net')
 const chatServer = net.createServer()
 
